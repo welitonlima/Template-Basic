be242fc9398177d060063cff8fc475017a4e987e
Weliton Pereira de Lima <welitonpl@gmail.com>
2013-05-17 17:48:57 -0300

    Fechamento tag

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html lang="pt-br">
 <head>
     <title>Title Site</title>
-	<meta charset="utf-8">    
+	<meta charset="utf-8" />
     <meta name="author" content="Weliton Lima" />
 	<meta name="keywords" content="css, folhas estilo cascata" />
-	<meta name="description" content="Tutoriais sobre CSS, XHTML" />	
+	<meta name="description" content="Tutoriais sobre CSS, XHTML" />
     <link type="text/css" rel="stylesheet" href="css/style.css?random=<?php echo rand(100,999) ?>" media="all" />
     <script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
     <script src="js/javascript.js" type="text/javascript"></script>
