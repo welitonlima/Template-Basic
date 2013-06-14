a0d7f0f863ed94f3020a84d6c37c58eb67ecb312
welitonlima <welitonpl@gmail.com>
2013-06-14 17:46:20 -0300

    index.php
    Adicionado 
    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8" />
     <meta name="author" content="Weliton Lima" />
     <meta name="keywords" content="css, folhas estilo cascata" />
-    <meta name="description" content="Tutoriais sobre CSS, XHTML" />
+    <meta name="description" content="Tutoriais sobre CSS, XHTML" />    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link type="text/css" rel="stylesheet" href="css/style.css?random=<?php echo rand(100,999) ?>" media="all" />
     <script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
     <script src="js/javascript.js" type="text/javascript"></script>
